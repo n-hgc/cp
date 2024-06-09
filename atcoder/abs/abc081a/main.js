43c95284a72c2ebfd9cb7936261f8874d9e41d5a
n-hgc <gpxxakag@gmail.com>
2024-06-09 16:15:54 +0900

    b問題まで

--- atcoder/abs/abc081a/main.js
+++ atcoder/abs/abc081a/main.js
@@ -1,6 +1,12 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    // 111 などの3桁の数字になるので1文字ずつ分割
+    const tmp = input[0].split('');
+    // 配列を数値に変更する
+    const intInput = tmp.map(str => parseInt(str,10));
+    // 配列の値の合計をする
+    const sums = intInput.reduce((prevVal, currVal) => prevVal + currVal, 0);
+    console.log(sums);
 }
 
-Main(require("fs").readFileSync("dev/stdin","utf8"));
+Main(require("fs").readFileSync("/dev/stdin","utf8"));
